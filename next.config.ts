d6564303602ff391d8f43178939b63e9f2a8de16
Nick Trinnaman <nicktrinnaman@Nicks-MacBook-Pro.local>
2026-03-24 16:08:26 +0100

    Remove wrong redirects from portal app

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,23 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/partner/:path*",
+        has: [
+          {
+            type: "host",
+            value: "camel-global.com",
+          },
+        ],
         destination: "https://portal.camel-global.com/partner/:path*",
         permanent: false,
       },
       {
         source: "/admin/:path*",
+        has: [
+          {
+            type: "host",
+            value: "camel-global.com",
+          },
+        ],
         destination: "https://portal.camel-global.com/admin/:path*",
         permanent: false,
       },
